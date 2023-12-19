f6a747729a6e8aa48cc51c9ff79768043ab00a25
Sadhgun Sobti <sadhgunsobti@gmail.com>
2023-12-19 19:19:56 +0530

    Created changes requisite for new folder required-files

--- qrcode-gen.py
+++ qrcode-gen.py
@@ -1,8 +1,12 @@
 import qrcode
+import os
 from PIL import ImageDraw, Image, ImageFont
 
 name = input("Enter name of study: ")
 pages = int(input("Enter number of pages: "))
+
+if not(os.path.exists(f"QRCodes/{name}")):
+        os.mkdir(f"QRCodes/{name}")
 
 for i in range(1, pages + 1):
     qr = qrcode.QRCode(
@@ -19,8 +23,8 @@ for i in range(1, pages + 1):
     box = Image.new(mode="RGB", size=(width, height + 70), color = (255, 255, 255)) #TODO: Check required resolution
     box.paste(img, (0, 0))
     draw = ImageDraw.Draw(box)
-    font = ImageFont.truetype(r'Roboto-Regular.ttf', 60)
+    font = ImageFont.truetype(r'required-files/Roboto-Regular.ttf', 60)
     dimensions = draw.textbbox((0, 0), f'Page No: {i}', font=font)
     draw.multiline_text(((width - dimensions[2]) // 2, height - 10), f'Page No: {i}', fill=0, font=font)
-    dest = f'QRCodes/Page{i}.png'
+    dest = f'QRCodes/{name}/Page{i}.png'
     box.save(dest)
