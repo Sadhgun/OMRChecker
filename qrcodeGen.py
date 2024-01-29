b94f8a15939628a7b8c939f521d7015fce6d96be
Sadhgun Sobti <sadhgunsobti@gmail.com>
2024-01-29 17:51:06 +0530

    Reformatted qrcode generator into function

--- qrcodeGen.py
+++ qrcodeGen.py
@@ -2,13 +2,7 @@ import qrcode
 import os
 from PIL import ImageDraw, Image, ImageFont
 
-name = input("Enter name of study: ")
-pages = int(input("Enter number of pages: "))
-
-if not(os.path.exists(f"QRCodes/{name}")):
-        os.mkdir(f"QRCodes/{name}")
-
-for i in range(1, pages + 1):
+def QRGenerate(name, i):
     qr = qrcode.QRCode(
         version=None, #TODO: Check version options
         error_correction=qrcode.constants.ERROR_CORRECT_H,
@@ -20,11 +14,21 @@ for i in range(1, pages + 1):
     qr.make()
     img = qr.make_image(fill_color="black", back_color="white")
     width, height = img.size
-    box = Image.new(mode="RGB", size=(width, height + 70), color = (255, 255, 255)) #TODO: Check required resolution
+    box = Image.new(mode="RGB", size=(width, height + 60), color = (255, 255, 255)) #TODO: Check required resolution
     box.paste(img, (0, 0))
     draw = ImageDraw.Draw(box)
     font = ImageFont.truetype(r'required-files/Roboto-Regular.ttf', 60)
     dimensions = draw.textbbox((0, 0), f'Page No: {i}', font=font)
     draw.multiline_text(((width - dimensions[2]) // 2, height - 10), f'Page No: {i}', fill=0, font=font)
-    dest = f'QRCodes/{name}/Page{i}.png'
-    box.save(dest)
+    return box
+
+if __name__ == "__main__":
+    name = input("Enter name of study: ")
+    pages = int(input("Enter number of pages: "))
+
+    if not(os.path.exists(f"QRCodes/{name}")):
+        os.mkdir(f"QRCodes/{name}")
+    for i in range(1, pages + 1):
+        box = QRGenerate(name, i)
+        dest = f'QRCodes/{name}/Page{i}.png'
+        box.save(dest)
